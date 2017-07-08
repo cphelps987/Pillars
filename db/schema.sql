39df3730939949a1c1b7803b42299cfb53a2b99d
gabietaylor <gabie.taylor@yahoo.com>
2017-07-08 19:31:39 -0400

    admin table created

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,14 @@
+-- Admin DB
 CREATE DATABASE adminDB;
 
 USE adminDB;
+
+CREATE TABLE adminTable
+(
+	id int NOT NULL AUTO_INCREMENT,
+	userNames varchar(255) NOT NULL,
+	verifiedUsers varchar(255) NOT NULL,
+	helpfulRes varchar(255) NOT NULL,
+	flags INT AUTO_INCREMENT NOT NULL,
+	PRIMARY KEY (id)
+);
