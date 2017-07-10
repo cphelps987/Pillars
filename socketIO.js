c303186fff168d84e91251f2b99234cfb558d8d7
gabietaylor <gabie.taylor@yahoo.com>
2017-07-10 18:47:20 -0400

    fixed errors

--- socketIO.js
+++ socketIO.js
@@ -1,11 +1,11 @@
 var express = require('express');
 
-module.exports = function (app) {
+// module.exports = function (app) {
 
   var app = require('express')();
   var http = require('http').Server(app);
   var io = require('socket.io')(http);
-  var port = process.env.PORT || 3000;
+  var port = process.env.PORT || 8080;
 
   app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/socketTest.html');
@@ -20,5 +20,3 @@ module.exports = function (app) {
   http.listen(port, function(){
     console.log('listening on :' + port);
   });
-
-}
