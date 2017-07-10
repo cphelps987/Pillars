55ae0081c39495b86036318567be45063d239ff3
Courtney Phelps <cphelps987@gmail.com>
2017-07-10 14:05:17 -0400

    Update socketIO.js

--- socketIO.js
+++ socketIO.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
   var app = require('express')();
   var http = require('http').Server(app);
   var io = require('socket.io')(http);
-  var port = process.env.PORT || 8080;
+  var port = process.env.PORT || 3000;
 
   app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/socketTest.html');
