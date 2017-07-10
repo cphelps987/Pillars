4b4855d794fc60dee0adb3ec38956ecd8f3badc2
Magus Dethen <maguslod@gmail.com>
2017-07-10 18:10:38 -0400

    Got chat to show up in chattest

--- server.js
+++ server.js
@@ -19,14 +19,14 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 // keeps user logged in
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true}));
+//app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true}));
 
 //flash to show a message if there's an incorrect login
-app.use(flash());
+//app.use(flash());
 
 //passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 exports.orm = require('./config/orm.js');
 
