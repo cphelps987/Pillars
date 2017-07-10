55e2e1872737c5a45c3687b760ec2448dea53169
gabietaylor <gabie.taylor@yahoo.com>
2017-07-10 19:46:01 -0400

    add new cols to chatDB

--- db/chatDB.sql
+++ db/chatDB.sql
@@ -8,5 +8,7 @@ CREATE TABLE chatTable
 	id int NOT NULL AUTO_INCREMENT,
 	link varchar(255) NOT NULL,
 	title varchar(255) NOT NULL,
+	moderators varchar(255) NOT NULL,
+	moderation level varchar(255) NOT NULL,
 	PRIMARY KEY (id)
 );
