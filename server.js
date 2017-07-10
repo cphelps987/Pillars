f9f7e45f138e835624ea2f18f1153ef5afa6e9ac
Sakina Shivji <shivjisakina@gmail.com>
2017-07-10 19:39:29 -0400

    cleaned server and added passport require

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 //express and body parser
-var express = require ("express");
+var express = require("express");
 var flash = require('connect-flash');
-var bodyParser = require ("body-parser");
-var exphbs = require ("express-handlebars");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 var session = require('express-session');
-
+var passport = require('passport');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main"}));
-app.set("view engine","handlebars");
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 //bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 
 // keeps user logged in
 //app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true}));
@@ -35,15 +35,12 @@ exports.orm = require('./config/orm.js');
 
 var routes = require('./controllers/controllers.js');
 
-require('./passport.js')
+require('./passport.js');
+require('./emoji.js');
+require('./chalk.js');
 // require('./socketIO.js')
 
-
 app.use("/", routes);
-
-require('./emoji.js');
-
-require('./chalk.js');
 
 app.listen(port, function () {
     console.log("Listening on PORT " + port);
