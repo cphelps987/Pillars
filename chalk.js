97646d784862ec28b0591eaf57c709edfd5e6fc7
meldelapaz <meldelapaz.17@gmail.com>
2017-07-10 11:43:51 -0400

    removing .idea

--- chalk.js
+++ chalk.js
@@ -4,10 +4,15 @@ module.exports = function (app) {
 
     const ctx = new chalk.constructor({enabled: false});
 
-    //const ctx = new chalk.constructor({level: 0});
+    //const ctxs = new chalk.constructor({level: 0});
 
 };
 
 console.log(chalk.blue('Hello world!'));
 
-console.log(chalk.red('Fuck you, it works'));
+console.log(chalk.red('Fuck you, chalk works'));
+
+console.log(chalk.green('Fuck you, chalk works'));
+
+//console.log(chalk.pink('Fuck you, chalk works'));
+
