35afd002e328d568e94f0fbad6f5778f11b0976f
gabietaylor <gabie.taylor@yahoo.com>
2017-07-10 20:19:04 -0400

    added tables

--- db/notsonice.sql
+++ db/notsonice.sql
@@ -5,6 +5,7 @@ USE notsoniceDB;
 CREATE TABLE notsoniceTable
 (
 	id int NOT NULL AUTO_INCREMENT,
-	
+	wordlist varchar(255) NOT NULL,
+	category (racist, offensive, sexist) varchar(255) NOT NULL,
 	PRIMARY KEY (id)
 );
