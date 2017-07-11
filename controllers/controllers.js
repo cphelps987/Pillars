1dec0592f99e25fc949a026b16f0ded028fec0ba
Magus Dethen <maguslod@gmail.com>
2017-07-10 21:19:48 -0400

    Created base handlebar pages and routes

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,29 +1,74 @@
 var express = require("express");
 var router = express.Router();
 
+//BASE
 router.get('/', function(req, res) {
 
   res.render("index.handlebars");
 
 });
 
+router.get('/about', function(req, res) {
+
+  res.render("about.handlebars");
+
+});
+
+router.get('/faq', function(req, res) {
+
+  res.render("about.handlebars");
+
+});
+
+router.get('/search', function(req, res) {
+
+  res.render("search.handlebars");
+
+});
+
+router.get('/termsofservice', function(req, res) {
+
+  res.render("termsofservice.handlebars");
+
+});
+
+router.get('/tos', function(req, res) {
+
+  res.render("termsofservice.handlebars");
+
+});
+
+//USER
 router.get('/admin', function(req, res) {
 
   res.render("admin.handlebars");
 
 });
 
+
+router.get('/user', function(req, res) {
+
+  res.render("user.handlebars");
+
+});
+
+
+//CHAT
 router.get('/plinth', function(req, res) {
 
   res.render("chat.handlebars");
 
 });
 
+
+//RESOURSES
 router.get('/chapiter', function(req, res) {
 
   res.render("resources.handlebars");
 
 });
+
+
 
 
 //TEST LINKS
