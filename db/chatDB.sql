c51ae29da277f599f89213a2258cc18c25c311fe
Sakina Shivji <shivjisakina@gmail.com>
2017-07-10 20:05:48 -0400

    fixed schema error and added mock data to chatDB_seeds.sql

--- db/chatDB.sql
+++ db/chatDB.sql
@@ -9,7 +9,7 @@ CREATE TABLE chatTable
 	link varchar(255) NOT NULL,
 	title varchar(255) NOT NULL,
 	moderators varchar(255) NOT NULL,
-	moderation level varchar(255) NOT NULL,
+	moderation_level varchar(255) NOT NULL,
 	flagged int NOT NULL,
 	password varchar(255) NOT NULL,
 	PRIMARY KEY (id)
