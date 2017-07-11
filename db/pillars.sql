05e83687820993b5c03fbe9911d2db15421bc460
meldelapaz <meldelapaz.17@gmail.com>
2017-07-11 10:19:14 -0400

    created ONE seeds file and ONE database

---
+++ db/pillars.sql
@@ -0,0 +1,63 @@
+CREATE DATABASE pillars;
+
+USE pillars;
+
+CREATE TABLE chatTable
+(
+	id int NOT NULL AUTO_INCREMENT,
+	link varchar(255) NOT NULL,
+	title varchar(255) NOT NULL,
+	moderators varchar(255) NOT NULL,
+	moderation_level varchar(255) NOT NULL,
+	flagged int NOT NULL,
+	password varchar(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE faqTable
+(
+	id int NOT NULL AUTO_INCREMENT,
+	questions varchar(255) NOT NULL,
+	answers varchar(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE linksTable
+(
+	id int NOT NULL AUTO_INCREMENT,
+	title varchar(255) NOT NULL,
+	description varchar(255) NOT NULL,
+	links varchar(255) NOT NULL,
+	userScore int NOT NULL,
+	facebook varchar(255) NULL,
+	twitter varchar(255) NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE notsoniceTable
+(
+	id int NOT NULL AUTO_INCREMENT,
+	wordlist varchar(255) NOT NULL,
+	category (racist, offensive, sexist) varchar(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE userTable
+(
+	id int NOT NULL AUTO_INCREMENT,
+	userName varchar(255) NOT NULL,
+	password varchar(255) NOT NULL,
+	role varchar(255) NOT NULL,
+	verifiedUser varchar(255) NOT NULL,
+	userScore int NOT NULL,
+	link varchar(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE `offensiveTable` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `word` varchar(255) NOT NULL,
+  `type` varchar(255) DEFAULT '',
+  PRIMARY KEY (`id`)
+);
