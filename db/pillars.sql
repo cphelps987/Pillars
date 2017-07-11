507dcb53b9767ae7f274b6c3842ea77c131919c5
Sakina Shivji <shivjisakina@gmail.com>
2017-07-11 19:27:52 -0400

    userTable role NOT NULL changed for passport

--- db/pillars.sql
+++ db/pillars.sql
@@ -39,7 +39,7 @@ CREATE TABLE userTable
 	id int NOT NULL AUTO_INCREMENT,
 	userName varchar(255) NOT NULL,
 	password varchar(255) NOT NULL,
-	role varchar(255) NOT NULL,
+	role varchar(255),
 	verifiedUser varchar(255),
 	userScore int,
 	link varchar(255),
