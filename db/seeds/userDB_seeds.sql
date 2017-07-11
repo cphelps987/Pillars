f7ea3618a411eb20648276e988ee38c108b1a7b9
Sakina Shivji <shivjisakina@gmail.com>
2017-07-11 08:50:45 -0400

    fixed userDB so i can integrate passport better

--- db/seeds/userDB_seeds.sql
+++ db/seeds/userDB_seeds.sql
@@ -1,10 +1,10 @@
-INSERT INTO `viewTable` (`id`, `userNames`, `password`, `verifiedUsers`, `role`, `score`, `link`, `flags`)
+INSERT INTO `userTable` (`id`, `userName`, `password`, `role`, `verifiedUser`, `userScore`, `link`)
 VALUES
-  (1, 'user1','password1','verified', 'user', '1', 'https://www.google1.com/', 1),
-  (2, 'user2','password2','notVerified', 'mod', '3', 'https://www.google2.com/', 4),
-  (3, 'user3','password3','verified', 'mod', '2', 'https://www.google3.com/', 2),
-  (4, 'user4','password4','verified', 'user', '4', 'https://www.google4.com/', 6),
-  (5, 'user5','password5','notVerified', 'user', '5', 'https://www.google5.com/', 3),
-  (6, 'user6','password6','verified', 'admin', '2', 'https://www.google6.com/', 7),
-  (7, 'user7','password7','verified', 'user', '7', 'https://www.google7.com/', 2);
+  (1, 'user1','password1', 'user', 'verified', '1', 'https://www.google1.com/'),
+  (2, 'user2','password2', 'mod', 'notVerified', '3', 'https://www.google2.com/'),
+  (3, 'user3','password3', 'mod', 'verified', '2', 'https://www.google3.com/'),
+  (4, 'user4','password4', 'user', 'verified', '4', 'https://www.google4.com/'),
+  (5, 'user5','password5', 'user', 'notVerified', '5', 'https://www.google5.com/'),
+  (6, 'user6','password6', 'admin', 'verified', '2', 'https://www.google6.com/'),
+  (7, 'user7','password7', 'user', 'verified', '7', 'https://www.google7.com/');
 
--- db/userDB.sql
+++ db/userDB.sql
@@ -8,7 +8,7 @@ CREATE TABLE userTable
 	id int NOT NULL AUTO_INCREMENT,
 	userName varchar(255) NOT NULL,
 	password varchar(255) NOT NULL,
-	role(user, admin, mod) varchar(255) NOT NULL,
+	role varchar(255) NOT NULL,
 	verifiedUser varchar(255) NOT NULL,
 	userScore int NOT NULL,
 	link varchar(255) NOT NULL,
