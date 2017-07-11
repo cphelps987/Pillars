38a338c7af630e808a133f2400cf2d7535722c14
cphelps987 <cphelps987@gmail.com>
2017-07-11 09:21:54 -0400

    fixed links db

--- db/linksDB.sql
+++ db/linksDB.sql
@@ -9,12 +9,13 @@ CREATE TABLE linksTable
 	title varchar(255) NOT NULL,
 	description varchar(255) NOT NULL,
 	links varchar(255) NOT NULL,
+	userScore int NOT NULL,
 	facebook varchar(255) NULL,
 	twitter varchar(255) NULL,
 	PRIMARY KEY (id)
 );
 
-INSERT INTO `linksTable` (`title`, `description`, `links`, `facebook`, `twitter`)
+INSERT INTO `linksTable` (`title`, `description`, `links`, `userScore`, `facebook`, `twitter`)
 VALUES
 ('Coming Out to Parents','Coming out to parents', 'http://www.huffingtonpost.com/2013/10/11/coming-out-resources_n_4085658.html'),
 ('Dating Violence','Dating Violence in Teens','https://www.cdc.gov/ViolencePrevention/intimatepartnerviolence/teen_dating_violence.html'),
