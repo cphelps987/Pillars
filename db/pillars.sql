963e8c90667b112ecc991aafb7cb8f831ef241d1
meldelapaz <meldelapaz.17@gmail.com>
2017-07-11 13:50:21 -0400

    changed database name

--- db/pillars.sql
+++ db/pillars.sql
@@ -38,7 +38,7 @@ CREATE TABLE notsoniceTable
 (
 	id int NOT NULL AUTO_INCREMENT,
 	wordlist varchar(255) NOT NULL,
-	category (racist, offensive, sexist) varchar(255) NOT NULL,
+	category varchar(255) NOT NULL,
 	PRIMARY KEY (id)
 );
 
