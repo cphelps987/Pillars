88890dcf3cfbd01a7999472781175e9120ae0f40
gabietaylor <gabie.taylor@yahoo.com>
2017-07-12 11:29:01 -0400

    socket merged to main

--- socketIO.js
+++ socketIO.js
@@ -27,14 +27,14 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
 // Routing
-app.use(express.static(__dirname + '/publicsocket'));
+app.use(express.static(__dirname + '/public'));
 /*  app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/socketTest.html');
   });
