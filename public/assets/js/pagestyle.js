b275c238b865fc892e8b471a3c74f936e5f119dd
Magus Dethen <maguslod@gmail.com>
2017-07-15 15:38:48 -0400

    Add plinth creation options

--- public/assets/js/pagestyle.js
+++ public/assets/js/pagestyle.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
 
   $(".moreInfo").hide();
-
 		$(".showMore").click(function() {
 				console.log("Clicked showMore");
 			$(".showMore").hide();    
 			$(".moreInfo").show();    
 		});
+
+	//Plinth Creation advance options
+  $("#advCreatePilinth").hide();
+		$(".showMore").click(function() {
+				console.log("Clicked showMore");
+			$("#createPilinth").hide("fast");    
+			$("#advCreatePilinth").show("slow", "swing");
+		});
+
+	//Moderation Level Checkbox Control
+	$("input.defaultUnselect").on('change', function() {
+			$("input.defaultSelect").not(this).prop("checked", false);  
+	});
+
+	$("input.defaultSelect").on('change', function() {
+			$("input.defaultUnselect").not(this).prop("checked", false);  
+	});
+
 });
