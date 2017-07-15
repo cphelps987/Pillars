6c81b25337ea56c204aeb79db6e1dbd40d26ae0a
Magus Dethen <maguslod@gmail.com>
2017-07-14 20:28:12 -0400

    Fixed password

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
         port    : 3306,
         host    : "localhost",
         user    : "root",
-        password: "magus",
+        password: "",
         database: "pillars"
     });
 }
