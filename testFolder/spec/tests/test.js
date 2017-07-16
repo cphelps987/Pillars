f4d4bfd3f7aed1f502b7e68e23fe20d8f33d183a
cphelps987 <cphelps987@gmail.com>
2017-07-16 17:13:37 -0400

    testing complete for the links for the page

--- testFolder/spec/tests/test.js
+++ testFolder/spec/tests/test.js
@@ -1,68 +1,64 @@
 /**
- * Created by courtneyphelps on 7/14/17.
+ * Created by courtneyphelps on 7/16/17.
  */
 var Nightmare = require("nightmare");
 
-describe("Pillars", function() {
-    // var login = "#header__sign-in";
+describe("Codecademy", function() {
+    // The default tests in mocha is 2 seconds.
+    // Extending it to 30 seconds to have time to load the pages
+    var login = "#header__sign-in";
 
-    it("should show home page", function(done) {
+    it("should require me to login", function(done) {
         // ID for the login button.
         Nightmare({ show: true })
-            .goto("http://localhost:3000/")
-            .wait(3000)
-            .click("#chapiterbutton")
-            .wait(3000)
-            .click("#dropdownmenu")
-            .wait(3000)
-            .click("#homebutton")
-            //.wait(300)
+            .goto("https://codecademy.com")
+            // Just to be safe.
+            .wait(login)
             // Click the login button.
-            //.click("#dropdownmenu")
-            .wait(3000)
-            //.click("#searchinput")
-            //.type("#seachinput","LGBT")
+            .click(login)
             // Evaluate the title
             .evaluate(function() {
+                throw new Error('I\'m an error!');
                 return document.title;
             })
 
             // Asset the title is as expected
             .then(function success(title) {
-                expect(title).toBe("Pillars");
+                expect(title).toBe("Log in | Codecademy");
                 done();
-            });/*function error(err) {
+            }, function error(err) {
                 throw err;
             });
-            done();*/
     }, 30000);
 
-   /* var login = "#dropdownnlogin";
-    it("should bring drop down login down to enter username/password", function(done) {
+    var login = "#header__sign-in";
+    it("should present a link to course catalog after login", function(done) {
         Nightmare({ show: true })
-            .goto("http://localhost:3000/login")
+            .goto("https://codecademy.com")
             // Just to be safe.
             .wait(login)
             // Click the login button.
             .click(login)
             // Wait for the login input
-            .wait("#UserLogin")
+            .wait("#user_login")
             // Actually log in
-            .type("#UserLogin", "user1")
-            .type("#UserPassword", "password1")
-            .click("#loginButton")
+            .type("#user_login", "ResilD")
+            .type("#user_password", "dummy*password")
+            .click("#user_submit")
             // Evaluate the following selector
             .evaluate(function() {
                 return document.querySelector("a[href='/learn/all']");
             })
             // Assert the catalog exists
-            .then(function() {
+            .then(function(catalog) {
+                expect(catalog).not.toBe(undefined);
                 done();
             });
     }, 30000);
-    /*it("should ", function() {
+
+    it("should ", function() {
         throw new Error(
             "Failed on purpose, just to make the Mocha output more interesting."
         );
-    });*/
+    });
 });
